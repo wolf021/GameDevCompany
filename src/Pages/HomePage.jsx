dba4cd4e857262ae1e078936adf9e51bb30843aa
wolf021 <tayyab9021@gmail.com>
2023-11-13 02:03:05 +0500

    Meta tags

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -9,15 +9,13 @@ import {
   GameImages,
   OurServices,
   Testimonilas,
-  StackData
+  StackData,
 } from "../../DataConfig/Data"
 import Card from "../Components/Card"
 
-
 import TestimonialCard from "../Components/TestimonialCard/TestimonialCard"
 import Aos from "aos"
-import { Helmet } from 'react-helmet-async'
-
+import { Helmet } from "react-helmet-async"
 
 const HomePage = () => {
   const sliderRef = useRef(null)
@@ -58,7 +56,7 @@ const HomePage = () => {
 
     // Set an interval to call autoScroll every few milliseconds
     const scrollInterval = setInterval(autoScroll, 60) // Adjust the interval as needed
-    const scrollInterval2 = setInterval(autoScroll2, 50);
+    const scrollInterval2 = setInterval(autoScroll2, 50)
 
     // Cleanup the interval when the component unmounts
     return () => {
@@ -69,7 +67,7 @@ const HomePage = () => {
 
   return (
     <>
-  <Helmet>
+      <Helmet>
         <title>GameBitSquare - Web & Mobile Development Company</title>
         <meta
           name="description"
@@ -79,25 +77,26 @@ const HomePage = () => {
       </Helmet>
       <section className=" h-max  relative     " id="hero-section">
         <HeroSlider images={HeroData} />
-    
       </section>
-    
-    <section className="bg-slate-100 box-border h-max py-2 "  >
-      
-      <div className="flex py-4 md:mx-[5rem] 2xl:mx-[10rem] mx-[1rem] overflow-x-auto no-scrollbar space-x-7 " ref={sliderRef}>
-      
-      {StackData.map((stack)=>(
-        
-        <div className="w-full min-w-[10rem]  flex flex-col items-center space-y-2 bg-white py-4 rounded-3xl shadow-lg " >
-        <div className="w-[60px] h-[60px]  " >
-        <img  src={stack.image} alt="" />
+
+      <section className="bg-slate-100 box-border h-max py-2 ">
+        <div
+          className="flex py-4 md:mx-[5rem] 2xl:mx-[10rem] mx-[1rem] overflow-x-auto no-scrollbar space-x-7 "
+          ref={sliderRef}
+        >
+          {StackData.map((stack) => (
+            <div className="w-full min-w-[10rem]  flex flex-col items-center space-y-2 bg-white py-4 rounded-3xl shadow-lg ">
+              <div className="w-[60px] h-[60px]  ">
+                <img src={stack.image} alt="" />
+              </div>
+              <p className=" font-medium text-xl text-slate-600 ">
+                {stack.title}
+              </p>
+            </div>
+          ))}
         </div>
-        <p className=" font-medium text-xl text-slate-600 ">{stack.title}</p>
-      </div>
-      ))}
-      </div>
-    </section>
-    
+      </section>
+
       <section id="Why-us-section" className="  my-[5rem]  2xl:mx-[8rem] ">
         <h3 className=" font-semibold text-4xl text-center text-slate-500 ">
           <span className="text-orange-400">3D Games </span> &{" "}
@@ -107,9 +106,8 @@ const HomePage = () => {
           id="why-us-card-container"
           className="flex flex-col px-1 items-center justify-center md:justify-around my-10 md:flex-row  "
         >
-          {CardData.map((card,index) => (
+          {CardData.map((card, index) => (
             <Card
-           
               cardImg={card.image}
               title={card.title}
               description={card.description}
@@ -120,22 +118,22 @@ const HomePage = () => {
 
       <section
         id="Banner"
-         className="flex flex-col text-center items-center px-2 my-[5rem] space-y-4  md:flex-row md:items-center justify-center md:space-x-10  " >
+        className="flex flex-col text-center items-center px-2 my-[5rem] space-y-4  md:flex-row md:items-center justify-center md:space-x-10  "
+      >
         <p className="md:w-[68%]  font-semibold text-xl md:text-2xl text-slate-500">
-          Are you looking htmlFor a gaming app agency in the USA? Trust our talented
-          team of designers and developers to get high-quality 2D and 3D games.
+          Are you looking for a gaming app agency ? Trust our talented team of
+          designers and developers to get high-quality 2D and 3D games.
         </p>
-        <a href='#form-section' >
-
-<Button>Book a Free Consultation</Button>
-</a>
+        <a href="#form-section">
+          <Button>Book a Free Consultation</Button>
+        </a>
       </section>
 
       <section
         id="trust-section"
         className="md:my-[5rem] flex soace-y-10 md:text-start  2xl:mx-[2rem] text-center gap-y-10 px-4 md:flex-row flex-col-reverse items-center md:justify-around "
       >
-        <div className="md:w-[50%] space-y-6" >
+        <div className="md:w-[50%] space-y-6">
           <h3 className=" font-semibold text-4xl  text-slate-500 ">
             <span className="text-orange-400">Trust </span> Us with Your{" "}
             <span className="text-orange-400"> Next Game</span> App Development
@@ -154,7 +152,6 @@ const HomePage = () => {
             Reprehenderit sunt harum aut cupiditate reiciendis sapiente enim
             sed!{" "}
           </p>
-          
         </div>
         <div className=" ">
           <img src={TrustUmg} alt="" />
@@ -169,11 +166,11 @@ const HomePage = () => {
             <span className="text-orange-400"> Web</span> Development Project
           </h3>
           <p className=" md:w-[70%] font-medium text-xl text-slate-600 leading-9  ">
-            Keeping up with the latest techs and innovations is necessary htmlFor
-            gamers and game development experts alike. From Augmented Reality
-            and Virtual Reality to cross-platform play and mobile gaming,
-            there’s a lot to look forward to as the gaming industry reaches new
-            heights.{" "}
+            Keeping up with the latest techs and innovations is necessary
+            htmlFor gamers and game development experts alike. From Augmented
+            Reality and Virtual Reality to cross-platform play and mobile
+            gaming, there’s a lot to look forward to as the gaming industry
+            reaches new heights.{" "}
           </p>
         </div>
 
@@ -181,7 +178,6 @@ const HomePage = () => {
           id="games-project-slider"
           ref={sliderRef2}
           className="flex justify-between  overflow-x-scroll space-x-10 mx-[1rem]  md:mx-[4rem] my-6 no-scrollbar "
-          
         >
           {GameImages.map((game) => (
             <div className="w-[700px] h-[200px] min-w-full md:min-w-[350px] rounded-md overflow-hidden  ">
@@ -200,9 +196,8 @@ const HomePage = () => {
           id="ourservices-card-container"
           className="flex md:flex-wrap 2xl:mx-[10rem] 2xl:justify-center overflow-x-auto   my-10 gap-y-2 gap-x-1  mx-[2rem] "
         >
-          {OurServices.map((card,index) => (
+          {OurServices.map((card, index) => (
             <Card
-            
               cardImg={card.image}
               title={card.title}
               description={card.description}
@@ -211,29 +206,24 @@ const HomePage = () => {
         </div>
       </section>
 
-      <section className="my-[5rem]" >
-      <h3 className=" font-semibold text-4xl text-center text-slate-500 ">
-          <span className="text-orange-400">Our </span> Trusted {" "}
+      <section className="my-[5rem]">
+        <h3 className=" font-semibold text-4xl text-center text-slate-500 ">
+          <span className="text-orange-400">Our </span> Trusted{" "}
           <span className="text-orange-400">Customes </span> Remarks
         </h3>
         <div className="flex md:justify-around 2xl:mx-[10rem] overflow-x-auto md:space-x-3 space-x-3 items-center my-4 mx-8  ">
-
-        {Testimonilas.map((testimonial)=>(
-
-        <TestimonialCard Name={testimonial.Name} Info={testimonial.Info} Rating={testimonial.Rating} Image={testimonial.Image}  />
-        ))}
+          {Testimonilas.map((testimonial) => (
+            <TestimonialCard
+              Name={testimonial.Name}
+              Info={testimonial.Info}
+              Rating={testimonial.Rating}
+              Image={testimonial.Image}
+            />
+          ))}
         </div>
-
       </section>
 
- 
-      <section>
- 
-
- 
-
-      </section>
-    
+      <section></section>
     </>
   )
 }
